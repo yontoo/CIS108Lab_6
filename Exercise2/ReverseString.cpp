62c415d0fdcbc8c7f1d1755a925216cb9d8d5b45
yontoo <traylee1513@yahoo.com>
2019-03-11 21:06:36 -0700

    Initial commit

---
+++ Exercise2/ReverseString.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <stack>
+#include <string>
+
+int main()
+{
+	std::string before_rev = "Invention, my dear friends, is 93% perspiration, 6% electricity, 4% evaporation, and 2% butterscotch ripple.";
+	char str_array[107];
+	std::stack<char> rev_stack;
+	std::strcpy(&str_array, before_rev.c_str);
+
+	return 0;
+}
